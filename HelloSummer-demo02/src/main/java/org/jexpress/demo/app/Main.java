03185e7f18f8279263c859a4c155496f4d4ab37d
Summer Boot Framework <ouleji@yahoo.com>
2023-05-02 21:09:24 -0400

    refactoring

--- HelloSummer-demo02/src/main/java/org/jexpress/demo/app/Main.java
+++ HelloSummer-demo02/src/main/java/org/jexpress/demo/app/Main.java
@@ -3,7 +3,7 @@ package org.jexpress.demo.app;
 import org.summerboot.jexpress.boot.SummerApplication;
 import org.summerboot.jexpress.boot.annotation.Version;
 
-@Version({"Hello Summer v1.0.0", SummerApplication.VERSION})
+@Version(value = {"Hello Summer v2.0.0", "jex32.3.6"}, logFileName="hellosummerv2")
 public class Main {
 
     public static void main(String... args) {
