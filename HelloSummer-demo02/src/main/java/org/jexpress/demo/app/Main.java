9691f80bd84f69e594f546f99e90031c79ce74a5
Summer Boot Framework <ouleji@yahoo.com>
2024-01-29 19:31:39 -0500

    clean up code

--- HelloSummer-demo02/src/main/java/org/jexpress/demo/app/Main.java
+++ HelloSummer-demo02/src/main/java/org/jexpress/demo/app/Main.java
@@ -1,14 +1,12 @@
 package org.jexpress.demo.app;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
 import org.summerboot.jexpress.boot.SummerApplication;
 import org.summerboot.jexpress.boot.annotation.Version;
 
 @Version(value = {"Hello Summer v2.0.0", "jex2.3.9"}, logFileName = "hellosummerv2")
 public class Main {
 
-    public static void main(String... args) throws URISyntaxException, IOException {
+    public static void main(String... args) {
         SummerApplication.run();
 //        SummerApplication app = SummerApplication.run();
 //        app.shutdown();
