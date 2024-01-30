fbadfc6536877486bc6d9e3ea609f7ed82dc08ad
Summer Boot Framework <ouleji@yahoo.com>
2024-01-30 06:45:54 -0500

    response with header and status code

--- MockService/src/main/java/org/jexpress/mockservice/app/MockServiceController.java
+++ MockService/src/main/java/org/jexpress/mockservice/app/MockServiceController.java
@@ -1,17 +1,26 @@
 package org.jexpress.mockservice.app;
 
 import com.google.inject.Singleton;
+import io.netty.handler.codec.http.HttpResponseStatus;
 import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PATCH;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Properties;
 import org.summerboot.jexpress.boot.annotation.Controller;
 import org.summerboot.jexpress.nio.server.domain.ServiceContext;
 
@@ -25,15 +34,56 @@ import org.summerboot.jexpress.nio.server.domain.ServiceContext;
 @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 public class MockServiceController {
 
-    @POST
+    // curl -k https://localhost:8211/service1/action1 -H "Accept: application/json" -H "Authorization: Bearer abcdefg"
+    // curl -k https://localhost:8211/service1/action1 -H "Accept: application/json" -H "Authorization: Bearer abcdefg" -X POST -d '{json}'
     @GET
+    @POST
+    @PUT
+    @PATCH
+    @DELETE
     @Path("")
     public String returnMockJsonContent(String body, @Parameter(hidden = true) final ServiceContext context) throws IOException {
-        return loadFile("mock_response" + context.uri() + "_" + context.method() + ".json");
+        String filePath = "mock_response" + context.uri() + "_" + context.method();
+        Properties responseHeaders = loadProperties(filePath + ".properties");
+        HttpResponseStatus status = setResponseHeaders(responseHeaders, context);
+        context.status(status);
+        return loadFileContent(filePath + ".txt");
     }
 
-    private String loadFile(String fileName) throws IOException {
+    protected Properties loadProperties(String fileName) throws IOException {
+        File fileEntry = new File(fileName).getAbsoluteFile();
+        if (!fileEntry.exists()) {
+            return null;
+        }
+        Properties props = new Properties();
+        try (InputStream is = new FileInputStream(fileEntry); InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8);) {
+            props.load(isr);
+        }
+        return props;
+    }
+
+    protected String loadFileContent(String fileName) throws IOException {
         File fileEntry = new File(fileName).getAbsoluteFile();
         return Files.readString(Paths.get(fileEntry.getAbsolutePath()));
+    }
+
+    protected final String KEY_RESPONSE_STATUS_CODE = "Response_Status_Code";
+
+    protected HttpResponseStatus setResponseHeaders(Properties responseHeaders, ServiceContext context) {
+        HttpResponseStatus status = HttpResponseStatus.OK;
+        if (responseHeaders == null) {
+            return status;
+        }
+        String responseStatusCode = responseHeaders.getProperty(KEY_RESPONSE_STATUS_CODE);
+        if (responseStatusCode != null) {
+            int code = Integer.parseInt(responseStatusCode);
+            status = HttpResponseStatus.valueOf(code);
+            responseHeaders.remove(KEY_RESPONSE_STATUS_CODE);
+        }
+        for (Object key : responseHeaders.keySet()) {
+            Object value = responseHeaders.get(key);
+            context.responseHeader(key.toString(), value);
+        }
+        return status;
     }
 }
